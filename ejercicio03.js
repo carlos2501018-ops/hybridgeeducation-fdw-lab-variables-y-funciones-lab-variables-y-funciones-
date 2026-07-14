145dae746e9b38d056e2a4f7c3f50e1b1208e54c
carlos2501018-ops <carlos2501018@hybridge.education>
2026-07-14 12:40:04 -0600

    Update ejercicio03.js

--- ejercicio03.js
+++ ejercicio03.js
@@ -1,8 +1,11 @@
 // Ejercicio 3: Suma de Dos Números
 // Declara una función llamada "sumar" que acepte dos parámetros (a y b) y retorne su suma
 
-//Escribe tu función aquí
-
+//Escribe tu función aquí 
+function sumar (){
+let x = 2, y = 3 ;    
+return x + y ;    
+}
 
 // Exporta la función para que pueda ser utilizada en las pruebas
 module.exports = {
